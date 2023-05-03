34cb6bb41d2ea01c8139a54c331db4bb4f63902e
Marco Galardini <marco.galardini@twincore.de>
2023-05-03 10:37:24 +0200

    Fix siftr install

--- workflow/scripts/install_siftr.R
+++ workflow/scripts/install_siftr.R
@@ -2,6 +2,8 @@
 
 Sys.setenv(TAR = "/bin/tar")
 
+Sys.unsetenv("GITHUB_PAT")
+
 if(!require("siftr", character.only = TRUE)){
   suppressPackageStartupMessages(library("devtools"))
   install_github("omarwagih/siftr")
