ce44da6386d58397c881fd7333de0e1efd629bb6
Bamu <fufordamaris@gmail.com>
2024-05-28 10:33:38 +0200

    modified manhattan_plot.py

--- workflow/scripts/manhattan_plot.py
+++ workflow/scripts/manhattan_plot.py
@@ -1,20 +1,21 @@
-import argparse
 import pandas as pd
 import matplotlib.pyplot as plt
 import subprocess
 import numpy as np
+import sys
 
 def get_options():
     description = 'Manhattan plot'
     parser = argparse.ArgumentParser(description=description)
     parser.add_argument('sample', help='Target mapped_all TSV file')
+    parser.add_argument('output', help='Output file for the Manhattan plot')
     return parser.parse_args()
 
-def manhattan_plot(data):
+def manhattan_plot(data, output):
     df = pd.read_csv(data, sep='\t', usecols=['strain', 'start', 'end', 'lrt-pvalue', 'gene'])
     df['pos'] = (df['start'] + df['end']) / (2 * 1_000_000)
     reference_strains = df['strain'].unique()
-    threshold = subprocess.check_output(['python3','workflow/scripts/count_patterns.py','--threshold', 'out/associations/phenotype/unitigs_patterns.txt'])
+    threshold = subprocess.check_output(['python3', 'workflow/scripts/count_patterns.py', '--threshold', 'out/associations/phenotype/unitigs_patterns.txt'])
     threshold = float(threshold)
 
     for reference in reference_strains:
@@ -26,9 +27,9 @@ def manhattan_plot(data):
         plt.xlabel('Genome Position')
         plt.ylabel('-log10(p-value)')
         plt.title(f'Manhattan Plot - {reference}')
-        plt.savefig('out/manhattan.png', dpi = 300)
+        plt.savefig(output, dpi=300)
 
 if __name__ == "__main__":
     args = get_options()
-    manhattan_plot(args.sample)
+    manhattan_plot(args.sample, args.output)
 
