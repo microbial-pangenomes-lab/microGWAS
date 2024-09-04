d0614da4fce449b788add787798d968372cc05a9
Marco Galardini <marco.galardini@twincore.de>
2024-09-04 13:00:29 +0200

    Forgot to add the 'strain' column

--- workflow/scripts/combine_panfeed.py
+++ workflow/scripts/combine_panfeed.py
@@ -16,6 +16,8 @@ def get_options():
                              '(the first column should contain the hashes)')
     parser.add_argument('kmers',
                         help='kmers_to_hashes.tsv file from panfeed')
+    parser.add_argument('patterns',
+                        help='hashes_to_patterns.tsv file from panfeed')
 
     parser.add_argument('--threshold',
                         type=float,
@@ -33,4 +35,22 @@ if __name__ == "__main__":
 
     k = pd.read_csv(options.kmers, sep='\t', index_col=2)
 
-    k.join(p, how='inner').to_csv(sys.stdout, sep='\t')
+    n = k.join(p, how='inner')
+    idx = set(n.index)
+
+    # we need to also load the patterns table to add the "strain" column
+    chunks = []
+    for chunk in pd.read_csv(options.patterns, sep='\t',
+                             index_col=0, chunksize=50000):
+        keep = idx.intersection(chunk.index)
+        if len(keep) > 0:
+            chunks.append(chunk.loc[sorted(keep)])
+    if len(chunks) == 0:
+        sys.exit(0)
+    h = pd.concat(chunks)
+    h = h.unstack()
+    h = h[h != 0]
+    h = h.reset_index().rename(columns={'level_0': 'strain'})
+
+    n.join(h.set_index('hashed_pattern')[['strain']],
+           how='inner').to_csv(sys.stdout, sep='\t')
