df946aaf20ad7e6cc84a459d83b801712b128d3b
mason-2000 <alemaso7777@gmail.com>
2026-04-02 15:19:27 +0200

    Bug fix

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -2,8 +2,6 @@
 
 set -e -u -o pipefail -x
 
-export CUDA_VISIBLE_DEVICES=1
-
 # Check if running in CI mode (explicit command line flag)
 CI_MODE=false
 for arg in "$@"; do
@@ -16,12 +14,11 @@ done
 
 # stop everything if we are risking to override
 # a genuine GWAS run
-# --- MODIFICA: Commentato per permettere di riprendere la run con cartella 'out' esistente ---
-# if [ -d "../out" ]; then
-#   echo "The output folder (../out) is present!";
-#   echo "Please remove it and run this script again";
-#   exit 1
-# fi
+if [ -d "../out" ]; then
+  echo "The output folder (../out) is present!"
+  echo "Please remove it and run this script again"
+  exit 1
+fi
 
 # Define absolute paths for configuration and data files
 CONFIG_BACKUP="$PWD/../config/config_backup.yaml"
@@ -38,7 +35,7 @@ cleanup() {
     [ -f "$CONFIG_BACKUP" ] && mv "$CONFIG_BACKUP" "$CONFIG_TEMP"
     [ -f "$DATA_BACKUP" ] && mv "$DATA_BACKUP" "$DATA_TEMP"
     
-    # Remove extracted directories
+    # Remove extracted directories (GFFs are now handled by ggcaller)
     rm -rf "${SMALL_GENOMES}small_fastas"
 }
 
@@ -58,45 +55,44 @@ if [ -f "../config/config.yaml" ]; then
     cp ../config/config.yaml ../config/config_backup.yaml
 fi
 
-# unpack the test dataset
+# unpack the test dataset (only fastas are needed now for ggcaller)
 tar -xvf small_fastas.tgz 
 
 # skip lineages estimation
 # these genomes are too small for it to work
 cp lineages.txt ../data/
-sed -i 's$lineages_file: \"out/lineages_mlst.txt\"$lineages_file: \"data/lineages.txt\"$g' ../config/config.yaml 
+sed -i 's$lineages_file: "out/lineages_mlst.txt"$lineages_file: "data/lineages.txt"$g' ../config/config.yaml 
 
 # download a smaller eggnog databse (gamma-proteobacteria)
-sed -i 's$eggnogdb: \"2\"$eggnogdb: \"1236\"$g' ../config/config.yaml 
+sed -i 's$eggnogdb: "2"$eggnogdb: "1236"$g' ../config/config.yaml 
 
 # ready to go
 cd ..
 
 # Also test the local assemblies functionality
-# --- MODIFICA: Commentato per evitare di rifare il setup dei reference e il bootstrap ---
-# zcat test/local_assemblies/536/genome.fasta.gz > test/local_assemblies/536/genome.fasta
-# zcat test/local_assemblies/536/genome.gff.gz > test/local_assemblies/536/genome.gff
-# zcat test/local_assemblies/536/genome.gbk.gz > test/local_assemblies/536/genome.gbk
-# #
-# if [ "$CI_MODE" = true ]; then
-#     # In CI mode, only use local references
-#     bash bootstrap.sh --local-dirs test/local_assemblies/536 Escherichia coli 536
-#     sed -i 's$enrichment_reference: \"IAI39\"$enrichment_reference: "536"$g' config/config.yaml 
-# else
-#     # In normal mode, use both local and remote references
-#     bash bootstrap.sh --local-dirs test/local_assemblies/536 Escherichia coli IAI39 GCF_000007445.1,GCF_000026305.1,GCF_000026265.1,GCF_000026345.1,GCF_000005845.2,GCF_000026325.1,GCF_000013265.1
-# fi
-#
-# # reduce the size of the reference proteome
-# # to speed up its annotation and rare variants analysis
-# cp test/reference.faa data/
+zcat test/local_assemblies/536/genome.fasta.gz > test/local_assemblies/536/genome.fasta
+zcat test/local_assemblies/536/genome.gff.gz > test/local_assemblies/536/genome.gff
+zcat test/local_assemblies/536/genome.gbk.gz > test/local_assemblies/536/genome.gbk
+
+if [ "$CI_MODE" = true ]; then
+    # In CI mode, only use local references
+    bash bootstrap.sh --local-dirs test/local_assemblies/536 Escherichia coli 536
+    sed -i 's$enrichment_reference: "IAI39"$enrichment_reference: "536"$g' config/config.yaml 
+else
+    # In normal mode, use both local and remote references
+    bash bootstrap.sh --local-dirs test/local_assemblies/536 Escherichia coli IAI39 GCF_000007445.1,GCF_000026305.1,GCF_000026265.1,GCF_000026345.1,GCF_000005845.2,GCF_000026325.1,GCF_000013265.1
+fi
+
+# reduce the size of the reference proteome
+# to speed up its annotation and rare variants analysis
+cp test/reference.faa data/
 
 # first dry run
-snakemake -np annotate_summary find_amr_vag map_back manhattan_plots heritability enrichment_plots qq_plots tree --cores 18 --use-conda
+snakemake -np annotate_summary find_amr_vag map_back manhattan_plots heritability enrichment_plots qq_plots tree --cores 8 --use-conda
 
 if [ "$CI_MODE" = true ]; then
     echo "CI mode: skipping actual snakemake run"
 else
     # actual run (brace yourself)
-    snakemake -p annotate_summary find_amr_vag map_back manhattan_plots heritability enrichment_plots qq_plots tree --cores 18 --use-conda
+    snakemake -p annotate_summary find_amr_vag map_back manhattan_plots heritability enrichment_plots qq_plots tree --cores 8 --use-conda
 fi
