9ad62d2fd32f1c50c45801c2d9f3d53d6c35d487
Marco Galardini <marco.galardini@protonmail.com>
2026-05-21 14:17:49 +0200

    Version bump

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,8 +6,8 @@ project = 'microGWAS'
 copyright = '2024, Microbial Pangenomes Lab'
 author = 'Microbial Pangenomes Lab'
 
-release = '0.7'
-version = '0.7.0'
+release = '0.8'
+version = '0.8.0'
 
 # -- General configuration
 
